f8f3f915d8c2141403f2cdba209e21db0c06f3cb
lmobarki <143443056+lmobarki@users.noreply.github.com>
2025-04-03 21:16:36 -0500

    fixing the dates

--- js/site.js
+++ js/site.js
@@ -1,21 +1,39 @@
 const newsData = [
-  { title: "New Anti-Cheat Update Released", date: "April 3, 2025" },
-  { title: "Scheduled Maintenance: April 5", date: "April 2, 2025" },
-  { title: "PB Now Supports More Games!", date: "March 30, 2025" },
-  { title: "Security Patch Deployed", date: "March 27, 2025" },
-  { title: "Player Ban Wave Announced", date: "March 25, 2025" },
-  { title: "Scroll Feature Added", date: "March 22, 2025" },
-  { title: "Improved UI Experience", date: "March 20, 2025" }
+  {
+    dateTime: "Mon Dec 16 2019 0:30",
+    message: "We have confirmed that Violation #74006 may be triggered by non-cheat software."
+  },
+  {
+    dateTime: "Wed Jun 24 2015 14:15",
+    message: "Version 2.352 of the PB Client for BF3 has been released to our PB Master Servers for auto-update and to our website download page."
+  },
+  {
+    dateTime: "Wed Jun 10 2015 11:00",
+    message: "We have confirmed that long standing Violation #51048 may be triggered by newly released non-cheat software."
+  },
+  {
+    dateTime: "Tue Apr 14 2015 14:00",
+    message: "We have confirmed that Violation #79623 may be triggered by non-cheat software."
+  },
+  {
+    dateTime: "Tue Apr 14 2015 14:00",
+    message: "We have confirmed that Violation #79623 may be triggered by non-cheat software."
+  },
+  {
+    dateTime: "Tue Apr 14 2015 0:00",
+    message: ""
+  }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
   const newsList = document.getElementById("newsList");
 
-  newsData.forEach((item, index) => {
+  newsData.forEach((item) => {
     const li = document.createElement("li");
-    li.className = "news-item" + (index % 2 === 0 ? " even" : " odd");
-    li.innerHTML = `<strong>${item.date}</strong><br>${item.title}`;
+    li.className = "news-item";
+    li.innerHTML = `<strong>${item.dateTime}</strong><br>${item.message}`;
     newsList.appendChild(li);
   });
 });
+
 
