fba484a9062987e8b5da574a17de5b76bd56349f
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2025-04-10 22:47:34 -0500

    adding javascript form submission

--- js/site.js
+++ js/site.js
@@ -1,1 +1,12 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
 
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      alert("Thank You, Your PunkBuster License Inquiry has been submitted!");
+
+    });
+  });
+</script>
