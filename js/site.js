01282f9486b584052ebf42e64820d212a975102b
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2025-04-28 13:20:09 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,10 +1,16 @@
 const newsData = [
-  { dateTime: "Mon Dec 16 2019 0:30", message: "We have confirmed that Violation #74006 may be triggered by non-cheat software." },
-  { dateTime: "Wed Jun 24 2015 14:15", message: "Version 2.352 of the PB Client for BF3 has been released to our PB Master Servers for auto-update and to our website download page." },
-  { dateTime: "Wed Jun 10 2015 11:00", message: "We have confirmed that long standing Violation #51048 may be triggered by newly released non-cheat software." },
-  { dateTime: "Tue Apr 14 2015 14:00", message: "We have confirmed that Violation #79623 may be triggered by non-cheat software." },
-  { dateTime: "Tue Apr 14 2015 14:00", message: "We have confirmed that Violation #79623 may be triggered by non-cheat software." },
-  { dateTime: "Tue Apr 14 2015 0:00", message: "" }
+  { dateTime: "Mon Dec 16 2019 0:30", 
+  message: "We have confirmed that Violation #74006 may be triggered by non-cheat software." },
+  { dateTime: "Wed Jun 24 2015 14:15", 
+  message: "Version 2.352 of the PB Client for BF3 has been released to our PB Master Servers for auto-update and to our website download page." },
+  { dateTime: "Wed Jun 10 2015 11:00", 
+  message: "We have confirmed that long standing Violation #51048 may be triggered by newly released non-cheat software." },
+  { dateTime: "Tue Apr 14 2015 14:00",
+  message: "We have confirmed that Violation #79623 may be triggered by non-cheat software." },
+  { dateTime: "Tue Apr 14 2015 14:00", 
+  message: "We have confirmed that Violation #79623 may be triggered by non-cheat software." },
+  { dateTime: "Tue Apr 14 2015 0:00", 
+  message: "" }
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
